766fab7589ebc1fb6ebf34dc6f0eed217b2fe760
LeonidPirogov <lv.pirogof@gmail.com>
2025-04-13 10:30:03 +0300

    Initial Commit

---
+++ viewScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewScreen
+//
+//  Created by Leonid on 13.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
